bcc8228c91ef54a93f17daeed13ae201b1d8a6f8
Icey <cshao_icey@qibaodwight.org>
2022-03-04 19:58:54 +0800

    swerve 改完了，把PID参数区分开

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,6 +72,52 @@ public final class Constants {
     public static final double kMaxSpeedMetersPerSecond = 3;
   }
 
+  public static final class PIDConfigConstants {
+    public static final double kpRearLeftDrive = 0.0;
+    public static final double kiRearLeftDrive = 0.0;
+    public static final double kdRearLeftDrive = 0.0;
+    public static final double kfRearLeftDrive = 0.0;
+
+    public static final double kpRearLeftTurn = 0.0;
+    public static final double kiRearLeftTurn = 0.0;
+    public static final double kdRearLeftTurn = 0.0;
+    public static final double kfRearLeftTurn = 0.0;
+
+    public static final double kpFrontLeftDrive = 0.0;
+    public static final double kiFrontLeftDrive = 0.0;
+    public static final double kdFrontLeftDrive = 0.0;
+    public static final double kfFrontLeftDrive = 0.0;
+
+    public static final double kpFrontLeftTurn = 0.0;
+    public static final double kiFrontLeftTurn = 0.0;
+    public static final double kdFrontLeftTurn = 0.0;
+    public static final double kfFrontLeftTurn = 0.0;
+
+    public static final double kpFrontRightDrive = 0.0;
+    public static final double kiFrontRightDrive = 0.0;
+    public static final double kdFrontRightDrive = 0.0;
+    public static final double kfFrontRightDrive = 0.0;
+
+    public static final double kpFrontRightTurn = 0.0;
+    public static final double kiFrontRightTurn = 0.0;
+    public static final double kdFrontRightTurn = 0.0;
+    public static final double kfFrontRightTurn = 0.0;
+    
+    public static final double kpRearRightDrive = 0.0;
+    public static final double kiRearRightDrive = 0.0;
+    public static final double kdRearRightDrive = 0.0;
+    public static final double kfRearRightDrive = 0.0;
+
+    public static final double kpRearRightTurn = 0.0;
+    public static final double kiRearRightTurn = 0.0;
+    public static final double kdRearRightTurn = 0.0;
+    public static final double kfRearRightTurn = 0.0;
+
+    
+    
+    
+  }
+
   public static final class ModuleConstants {
     public static final double kMaxModuleAngularSpeed = 2250;
     public static final double kMaxModuleAngularAcceleration = 2250;
